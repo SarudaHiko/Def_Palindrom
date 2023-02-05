bd93d4b6dc201e634335870aeaf8ba63eca477f9
Saruda <98002516+SarudaHiko@users.noreply.github.com>
2023-02-05 19:50:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ def de(text):
     text = text.replace(' ', '')
     if text == text[::-1]:
         print('Ваш текст является палиндромом')
+    # elif text == '':
+    #     print('Ничего не введено')
+    #     de(input('Введите любой текст: \n'))
     else:
         print('Ваш текст не является палиндромом')
 
